7b3cf3c487862032c934bc60d1d3a30513d2f3b8
cuteprettykara <cuteprettykara@gmail.com>
2019-04-15 17:22:19 +0900

    debug 정보 추가

--- src/main/java/org/zerock/util/UploadUtils.java
+++ src/main/java/org/zerock/util/UploadUtils.java
@@ -1,6 +1,7 @@
 package org.zerock.util;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.ImagingOpException;
 import java.io.File;
 import java.io.IOException;
 import java.text.DecimalFormat;
@@ -92,21 +93,28 @@ public class UploadUtils {
 		log.info("* uploadPath: {}", uploadPath);
 		log.info("* path: {}", path);
 		log.info("* fileName: {}", fileName);
-		BufferedImage sourceImg = ImageIO.read(new File(uploadPath + path, fileName));
-		log.info("makeThumbnail - 1");
-		BufferedImage destImg = Scalr.resize(sourceImg, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_TO_HEIGHT, 100);
-		log.info("makeThumbnail - 2");
-		String thumbnailName = uploadPath + path + File.separator + "s_" + fileName;
-		log.info("makeThumbnail - 3");
-		File newFile = new File(thumbnailName);
-		String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
-		log.info("makeThumbnail - 4");
-		ImageIO.write(destImg, formatName.toUpperCase(), newFile);
-		log.info("makeThumbnail - 5");
 		// 브라우저에서 윈도우의 경로로 사용하는 '\' 문자가 정상적인 경로로 인식되지 않기 때문에 '/'로 치환해 줍니다.
-		String replacedName = thumbnailName.substring(uploadPath.length()).replace(File.separatorChar, '/');
-		log.info("thumbnailName: {}", thumbnailName);
-		log.info("replacedName : {}", replacedName);
+		String replacedName = null;
+		try {
+			BufferedImage sourceImg = ImageIO.read(new File(uploadPath + path, fileName));
+			log.info("makeThumbnail - 1");
+			BufferedImage destImg = Scalr.resize(sourceImg, Scalr.Method.AUTOMATIC, Scalr.Mode.FIT_TO_HEIGHT, 100);
+			log.info("makeThumbnail - 2");
+			String thumbnailName = uploadPath + path + File.separator + "s_" + fileName;
+			log.info("makeThumbnail - 3");
+			File newFile = new File(thumbnailName);
+			String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+			log.info("makeThumbnail - 4");
+			ImageIO.write(destImg, formatName.toUpperCase(), newFile);
+			log.info("makeThumbnail - 5");
+			replacedName = thumbnailName.substring(uploadPath.length()).replace(File.separatorChar, '/');
+			log.info("thumbnailName: {}", thumbnailName);
+			log.info("replacedName : {}", replacedName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw(e);
+		}
 		
 		return replacedName;
 	}

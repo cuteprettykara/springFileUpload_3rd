444022c02dbac39e432a41c61e139078536c0aec
cuteprettykara <cuteprettykara@gmail.com>
2019-04-16 09:48:52 +0900

    전송된 파일을 화면에 표시하기(파일데이터를 브라우저로 전송하기)

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -1,12 +1,18 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.util.MediaUtils;
 import org.zerock.util.UploadUtils;
 
 @Controller
@@ -69,5 +76,50 @@ public class UploadController {
 		String savedName = UploadUtils.uploadFile(UPLOAD_PATH, file.getOriginalFilename(), file.getBytes());
 		
 		return new ResponseEntity<String>(savedName, HttpStatus.CREATED);
+	}
+	
+	@ResponseBody
+	@RequestMapping("displayFile")
+	public ResponseEntity<byte[]> displayFile(String fileName) throws IOException {
+		
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		
+		log.info("FILE NAME: {}", fileName);
+		
+		String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+		
+		MediaType mType = MediaUtils.getMediaType(formatName);
+		
+		HttpHeaders headers = new HttpHeaders();
+		
+		try {
+			in = new FileInputStream(UPLOAD_PATH + fileName);
+			
+			if (mType != null) {
+				// MIME 타입을 이미지 타입으로 설정
+				headers.setContentType(mType);
+			} else {
+				// MIME 타입을 다운로드용으로 설정
+				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+				fileName = fileName.substring(fileName.indexOf("_") + 1);
+				
+				// 파일의 이름이 한글일 경우 깨지지 않도록 한글 처리 인코딩을 한다.
+				String encodedFileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+				
+				headers.add("Content-Disposition", "attachment; filename=\"" + encodedFileName + "\"");
+			}
+			
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		} finally {
+			in.close();
+		}
+		
+		return entity;
 	}
 }
